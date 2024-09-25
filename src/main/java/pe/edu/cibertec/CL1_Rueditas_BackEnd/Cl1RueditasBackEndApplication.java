5fd28c6bc4d39a3cedcad777ff74669eee2b0e43
Rodríguez Jheicer <jheicer.ro@gmail.com>
2024-09-25 15:45:53 -0500

    Cl1-Creación del repositorio

---
+++ src/main/java/pe/edu/cibertec/CL1_Rueditas_BackEnd/Cl1RueditasBackEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_Rueditas_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1RueditasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1RueditasBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL1_Rueditas_BackEnd/Cl1RueditasBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL1_Rueditas_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1RueditasBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
